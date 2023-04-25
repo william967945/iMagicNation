48410b1c2d4c36afb6a5dc3ebe1835ce79b0aec0
tsaiiuo <tsaiiuo1@gmail.com>
2023-04-26 04:24:18 +0800

    1

--- src/common/db.js
+++ src/common/db.js
@@ -1,6 +1,27 @@
-// const config = require('config');
-const MySql = require("../lib/mysql");
-const config = require("config");
+import config from "config";
+import Sequelize from "sequelize";
 
 //FIXME: 建議使用config帶入MySQL連線設置
-module.exports = new MySql(config.get("MySQL"));
+// export default new MySql({
+//   "connectionLimit": 10, //同時MySQL連線數上限
+//   "host": config.mysql.host,
+//   "user": config.mysql.username,
+//   "password": config.mysql.password,
+//   "database": config.mysql.database,
+//   "charset": "utf8",
+//   "multipleStatements": true //支援以分號分隔的多筆查詢語法與否
+// });
+
+export default new Sequelize(
+  "", // DB schema
+  "", // DB username
+  "", // DB password
+  {
+    host: "",
+    dialect: "mysql",
+    dialectOptions: {
+      charset: "utf8",
+      multipleStatements: true,
+    },
+  }
+);
